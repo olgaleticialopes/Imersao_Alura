52777eb4757bdf627c8455584296537ade898021
OLGALETICIALOPES <leticiaolgalopes@gmail.com>
2023-06-09 00:45:29 -0300

    AluraProjetos

---
+++ AluraFlix/index.js
@@ -0,0 +1,13 @@
+
+var filmesleti = ['https://br.web.img3.acsta.net/pictures/22/11/09/21/57/2015334.jpg', 
+'https://i.pinimg.com/564x/b0/ed/7a/b0ed7a878af8378e0ae78ed0ed39a0fe.jpg',
+'https://i.pinimg.com/564x/3e/d0/de/3ed0ded01b0dee6ad943a020df8b95d2.jpg', 
+'https://br.web.img2.acsta.net/c_310_420/pictures/21/10/13/21/56/4755833.jpg',
+'https://br.web.img3.acsta.net/pictures/22/06/23/21/10/1998178.jpg',
+'https://trilhadomedo.com/wp-content/uploads/2020/12/WRONG-TURN-20_1sht_DIGITAL_FIN.jpg',
+ 'https://br.web.img2.acsta.net/medias/nmedia/18/89/56/94/20055685.jpg']
+
+for(var i=0; i<filmesleti.length; i++){
+  document.write('<img src='+ filmesleti[i]+ '>')
+}
+
